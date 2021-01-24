fdcd1ceb4464313eb4c4c38bab92530db464ee22
Kyle McDowell <kymcd21@icstudents.org>
2021-01-24 10:09:00 +0000

    day2

--- King.java
+++ King.java
@@ -1,6 +1,6 @@
 public class King extends Queen {
   public void divide1(int random1, int random2) {
-    int output = random1 / random2;
+    int output = random2 / random1;
     System.out.println(output);
   }
 }
